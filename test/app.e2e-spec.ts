bc8b4bf683c73f65e6d2e69ac6507c65278dade8
Bao Phu Tuong <phutuongbao1999@gmail.com>
2023-03-01 00:07:13 +0700

    Setting Up Testing (Path Error)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common/interfaces';
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    describe('SignUp', () => {
+      it.todo('Should Sign Up');
+    });
+
+    describe('Login', () => {
+      it.todo('Should Login');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Me', () => {});
+
+    describe('Edit User', () => {});
+  });
+
+  describe('BookMark', () => {
+    describe('Create Bookmark', () => {});
+    describe('Get All Bookmarks', () => {});
+    describe('Get Bookmark by Id', () => {});
+    describe('Edit Bookmark', () => {});
+    describe('Delete Bookmark', () => {});
   });
 });
